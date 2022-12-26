11b00a5e14728d4e17582d169b8db9544e5f608a
Oderen <oleksandr.deren02@gmail.com>
2022-12-26 20:31:15 +0200

    commit

--- src/js/read-more-modal.js
+++ src/js/read-more-modal.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("read-more-modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
